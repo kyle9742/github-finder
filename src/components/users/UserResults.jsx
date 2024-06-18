a2f862d34b2f377c80a627701ca34220ef924473
it <kyle9742@naver.com>
2024-06-18 10:39:16 +0900

    user 검색 기능

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  const fetchUsers = async () => {
+    const response = await fetch("https://api.github.com/users", {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
-    <div>
-      
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {users.map((user) => (
+        <h3>{user.login}</h3>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default UserResults
+export default UserResults;
