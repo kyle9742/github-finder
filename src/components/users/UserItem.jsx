efa131168b3218ede708699a6f35dcbf6fb5c94e
it <kyle9742@naver.com>
2024-06-18 11:13:50 +0900

    로딩중 스피너 표시

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function UserItem(props) {
+  return (
+    <div>
+      유저
+    </div>
+  )
+}
+
+UserItem.propTypes = {
+
+}
+
+export default UserItem
+
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
     fetchUsers();
   }, []);
+  
   const fetchUsers = async () => {
     const response = await fetch("https://api.github.com/users", {
       headers: {
@@ -12,15 +16,21 @@ const UserResults = () => {
       },
     });
     const data = await response.json();
-    console.log(data);
+    setUsers(data);
+    setLoading(false);
   };
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+
+  if(!loading) {
+    return (
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+          {users.map((user) => (
+            <h3 key= {user.id}>{user.login}</h3>
+          ))}
+        </div>
+      );
+  } else {
+    return <Spinner />
+  }
 };
 
 export default UserResults;
