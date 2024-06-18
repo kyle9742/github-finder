fa52d48fe25baa5c5fe341012db4f3491db57493
it <kyle9742@naver.com>
2024-06-18 14:08:54 +0900

    import 수정

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useReducer } from "react";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
