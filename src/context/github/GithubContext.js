daf602fa0d90a041501f0c4f10e987d795458dae
it <kyle9742@naver.com>
2024-06-18 14:08:20 +0900

    useState 에서 reducer 로 변경

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,30 +7,47 @@ const GITHUB_URL = "https://api.github.com";
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    const fetchUsers = async () => {
-      const response = await fetch(`${GITHUB_URL}/users`, {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      });
-      const data = await response.json();
-  
-      setUsers(data);
-      setLoading(false);
-    };
-  
-    return (
-      <GithubContext.Provider value={{
-        users,
-        loading,
-        fetchUsers,
-      }}>
-        {children}
-      </GithubContext.Provider>
-    );
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  const initialState = {
+    user: [],
+    loading: true,
   };
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+
+    // setUsers(data);
+    // setLoading(false);
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        // users,
+        // loading,
+        // fetchUsers,
+
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
 export default GithubContext;
