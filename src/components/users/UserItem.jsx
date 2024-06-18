897a7bf0b00d3916cb70068018c23ce3e34eb86f
it <kyle9742@naver.com>
2024-06-18 11:24:28 +0900

    유저 아바타, 로그인 출력

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-function UserItem(props) {
+function UserItem({user: { avatar_url, login } }) {
   return (
-    <div>
-      유저
-    </div>
+    <div className='card shadow-md compact side bg-base-100'>
+			<div className='flex-row items-center space-x-4 card-body'>
+				<div>
+					<div className='avatar'>
+						<div className='rounded-full shadow w-14 h-14'>
+							<img src={ avatar_url } alt='Profile' />
+						</div>
+					</div>
+				</div>
+				<div>
+					<h2 className='card-title'>{ login }</h2>
+					<Link className='text-base-content text-opacity-40' to={`/user/${login}`}>
+						프로파일 보기
+					</Link>
+				</div>
+			</div>
+		</div>
   )
 }
 
-UserItem.propTypes = {
 
-}
 
 export default UserItem
 
